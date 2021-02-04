87720f0691446dc252bae11a6a5908c255a3cc2a
Miguel Dominguez <36123431+MiguelDominguezSanchez@users.noreply.github.com>
2021-02-04 20:40:41 +0100

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import BlogList from './BlogList';
 
 const Home = () => {
 	const [blogs, setBlogs] = useState([
-		{ title: 'title one', body: 'body one...', author: 'mario', id: 1 },
+		{ title: 'title one', body: 'lorem ipsum...one', author: 'mario', id: 1 },
 		{ title: 'title two', body: 'body two...', author: 'yoshi', id: 2 },
 		{ title: 'title three', body: 'body three...', author: 'mario', id: 3 },
 	]);
