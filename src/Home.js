cce5bb92d1951c7b185a90f2ad476d002a3b16ea
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-02-05 09:23:01 +0100

    #15 – useEffect Dependencies

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
 		{ title: 'title three', body: 'body three...', author: 'mario', id: 3 },
 	]);
 
+	const [name, setName] = useState('mario');
+
 	const handleDelete = id => {
 		const newBlogs = blogs.filter(blog => blog.id !== id);
 		setBlogs(newBlogs);
@@ -15,12 +17,14 @@ const Home = () => {
 
 	useEffect(() => {
 		console.log('use effect ran');
-		console.log(blogs);
+		console.log(name);
 	});
 
 	return (
 		<div className="home">
 			<BlogList blogs={blogs} title="All Blogs !" handleDelete={handleDelete} />
+			<button onClick={() => setName('luigi')}>Change Name</button>
+			<p>{name}</p>
 		</div>
 	);
 };
