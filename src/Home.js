b6222feaa3fed2f93979d22b34bc0a804bb0e6a3
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-02-06 18:24:52 +0100

    #18 – Conditional Loading Message

--- src/Home.js
+++ src/Home.js
@@ -2,29 +2,26 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-	const [blogs, setBlogs] = useState([
-		{ title: 'title one', body: 'lorem ipsum...one', author: 'mario', id: 1 },
-		{ title: 'title two', body: 'body two...', author: 'yoshi', id: 2 },
-		{ title: 'title three', body: 'body three...', author: 'mario', id: 3 },
-	]);
-
-	const [name, setName] = useState('mario');
-
-	const handleDelete = id => {
-		const newBlogs = blogs.filter(blog => blog.id !== id);
-		setBlogs(newBlogs);
-	};
+	const [blogs, setBlogs] = useState(null);
+	const [isPending, setIsPending] = useState(true);
 
 	useEffect(() => {
-		console.log('use effect ran');
-		console.log(name);
-	});
+		setTimeout(() => {
+			fetch('http://localhost:8000/blogs')
+				.then(res => {
+					return res.json();
+				})
+				.then(data => {
+					setBlogs(data);
+					setIsPending(false);
+				});
+		}, 1000);
+	}, []);
 
 	return (
 		<div className="home">
-			<BlogList blogs={blogs} title="All Blogs !" handleDelete={handleDelete} />
-			<button onClick={() => setName('luigi')}>Change Name</button>
-			<p>{name}</p>
+			{isPending && <div>loading...</div>}
+			{blogs && <BlogList blogs={blogs} title="All Blogs !" />}
 		</div>
 	);
 };
